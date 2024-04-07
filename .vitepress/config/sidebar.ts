871e58352ab2ecced1acb7e19800402d7a0a12d2
zhangkr <zhangkr>
2024-04-07 15:04:12 +0800

    1

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -8,10 +8,14 @@ export const sidebar = {
     {
       text: '快速上手',
       collapsible: true,
-      link: '/docs/部署',
+      link: '/docs/技术栈',
       items: [
+        { text: '技术栈', link: '/docs/技术栈' },
+        { text: '工具类', link: '/docs/工具类' },
+        { text: '组件库', link: '/docs/组件库' },
         { text: '部署', link: '/docs/部署' },
-        { text: '脚手架', link: '/docs/脚手架' }
+        { text: '脚手架', link: '/docs/脚手架' },
+        { text: 'Git使用规范', link: '/docs/Git使用规范' },
       ]
     }
   ]
