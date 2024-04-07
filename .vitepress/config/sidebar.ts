f382b606180661bc088b2e9120031813187e6e68
zhangkr <zhangkr>
2024-04-07 11:10:08 +0800

    1

--- .vitepress/config/sidebar.ts
+++ .vitepress/config/sidebar.ts
@@ -8,9 +8,8 @@ export const sidebar = {
     {
       text: '快速上手',
       collapsible: true,
-      link: '/docs/hello',
+      link: '/docs/部署',
       items: [
-        { text: 'hello', link: '/docs/hello' },
         { text: '部署', link: '/docs/部署' },
         { text: '脚手架', link: '/docs/脚手架' }
       ]
