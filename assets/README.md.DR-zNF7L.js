edabecaa47874d08d41ad6223f4ab07a77d55238
zkr-1211 <zkr-1211@users.noreply.github.com>
2024-04-09 01:15:21 +0000

    deploy: b00a925843b07256e3844f861b179df912384e90

---
+++ assets/README.md.DR-zNF7L.js
@@ -0,0 +1,11 @@
+import{_ as s,c as a,o as i,U as n}from"./chunks/framework.DlPsLRwt.js";const F=JSON.parse('{"title":"场景前端开发文档","description":"","frontmatter":{},"headers":[],"relativePath":"README.md","filePath":"README.md","lastUpdated":1712625238000}'),e={name:"README.md"},t=n(`<h1 id="场景前端开发文档" tabindex="-1">场景前端开发文档 <a class="header-anchor" href="#场景前端开发文档" aria-label="Permalink to &quot;场景前端开发文档&quot;">​</a></h1><h2 id="👀-启动" tabindex="-1">👀 启动 <a class="header-anchor" href="#👀-启动" aria-label="Permalink to &quot;👀 启动&quot;">​</a></h2><div class="language-shell vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">shell</span><pre class="shiki shiki-themes github-light github-dark vp-code"><code><span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 安装依赖</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpm</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> i</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 启动</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpm</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> dev</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 打包</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpm</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> build</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#6A737D;--shiki-dark:#6A737D;"># 预览打包</span></span>
+<span class="line"><span style="--shiki-light:#6F42C1;--shiki-dark:#B392F0;">pnpm</span><span style="--shiki-light:#032F62;--shiki-dark:#9ECBFF;"> serve</span></span></code></pre></div>`,3),l=[t];function p(h,r,d,k,c,o){return i(),a("div",null,l)}const g=s(e,[["render",p]]);export{F as __pageData,g as default};
